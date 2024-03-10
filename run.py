93251cb5ea505a0afd2b2715c5bc7885476e6838
XiaoxiaoChi-code <3219274995@qq.com>
2024-03-11 10:08:05 +1100

    write target model and to run

--- run.py
+++ run.py
@@ -39,13 +39,14 @@ if __name__ == "__main__":
     train_tgt = pd.read_csv('data/target_trainingData.csv')
     test_tgt = pd.read_csv('data/target_testingData.csv')
 
+
+    # training source model and store user embedding matrix and item embedding matrix
     train_src_array = train_src.to_numpy()
     users, items, ratings = train_src_array[:,0].tolist(), train_src_array[:,1].tolist(), train_src_array[:,2].tolist()
     users_tensor, items_tensor, ratings_tensor = torch.LongTensor(users), torch.LongTensor(items), torch.FloatTensor(ratings)
     dataset = UserItemRatingRatingDataset(user_tensor=users_tensor,
                                           item_tensor=items_tensor,
                                           rating_tensor=ratings_tensor)
-
     dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
 
     device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
@@ -73,14 +74,64 @@ if __name__ == "__main__":
 
         print("for epoch {}, the loss is {}".format(t, sum(loss_epoch)/ len(loss_epoch)))
 
+    # relatively storing item matrix and user matrix for source model
+    item_embedding_src = mf_model.item_embedding.weight.data.cpu()
+    item_embedding_dataFrame_src = pd.DataFrame(item_embedding_src.numpy())
+    item_embedding_dataFrame_src.to_csv('model_parameters/item_embedding_source_model.csv', index=False, header=None)
+
+    user_embedding_src = mf_model.user_embedding.weight.data.cpu()
+    user_embedding_dataFrame_src = pd.DataFrame(user_embedding_src.numpy())
+    user_embedding_dataFrame_src.to_csv('model_parameters/user_embedding_source_model.csv', index=False, header=None)
 
 
 
 
+    # training target model
+    train_tgt_array = train_tgt.to_numpy()
+    users_tgt, items_tgt, ratings_tgt = train_tgt_array[:, 0].tolist(), train_tgt_array[:, 1].tolist(), train_tgt_array[:,2].tolist()
+    users_tgt_tensor, items_tgt_tensor, ratings_tgt_tensor = torch.LongTensor(users_tgt), torch.LongTensor(items_tgt), torch.FloatTensor(ratings_tgt)
+    dataset_tgt = UserItemRatingRatingDataset(user_tensor=users_tgt_tensor,
+                                          item_tensor=items_tgt_tensor,
+                                          rating_tensor=ratings_tgt_tensor)
+    dataloader_tgt = DataLoader(dataset_tgt, batch_size=128, shuffle=True)
+
+    device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
+
+    num_users_tgt = train_tgt['user_id'].max()
+    num_items_tgt = len(train_tgt['book_id'].unique())
+    mf_model_tgt = MatrixFactorization(num_users_tgt + 1, num_items_tgt, num_emd=10).to(device)
+    loss_func_tgt = torch.nn.MSELoss()
+    optimizer_tgt = torch.optim.SGD(mf_model_tgt.parameters(), lr=0.01, momentum=0.9)
+
+    epoch = 50
+
+    for t in range(epoch):
+        loss_epoch = []
+        mf_model_tgt.train()
+        for _, (user, item, rating) in enumerate(dataloader_tgt):
+            user, item, rating = user.to(device), item.to(device), rating.to(device)
+            predicted_rating = mf_model_tgt(user, item)
+            loss = loss_func_tgt(predicted_rating, rating)
+            # print(loss.item())
+            loss_epoch.append(loss.item())
+            loss.backward()
+            optimizer_tgt.step()
+            optimizer_tgt.zero_grad()
+
+        print("for epoch {}, the loss is {}".format(t, sum(loss_epoch) / len(loss_epoch)))
 
 
+    # relatively storing user embedding matrix and item embedding matrix for target model
+    item_embedding_tgt = mf_model_tgt.item_embedding.weight.data.cpu()
+    user_embedding_tgt = mf_model_tgt.user_embedding.weight.data.cpu()
 
+    item_embedding_tgt_dataFrame = pd.DataFrame(item_embedding_tgt.numpy())
+    item_embedding_tgt_dataFrame.to_csv('model_parameters/item_embedding_target_model.csv', index=False, header=None)
 
+    user_embedding_tgt_dataFrame = pd.DataFrame(user_embedding_tgt.numpy())
+    user_embedding_tgt_dataFrame.to_csv('model_parameters/user_embedding_target_model.csv', index=False, header=None)
+
+    
 
     # print("the max number of user in source domain is {}".format(train_src['user_id'].max()))
     # print("there are total {} users in source domain".format(len(train_src['user_id'].unique())))
